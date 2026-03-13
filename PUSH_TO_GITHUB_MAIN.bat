d8adef99c1a7ffc60e7656ce4b3455cae82bc81e
“espetinhosofiate367” <espetinhosofiate367@gmail.com>
2026-03-14 00:49:11 +0800

    Add GitHub helper launchers

---
+++ PUSH_TO_GITHUB_MAIN.bat
@@ -0,0 +1,5 @@
+@echo off
+cd /d "C:\Users\SWH\Desktop\GitHub_Docs_Package\Code_Archive\paper_flexible_sensor_lung_nodule_localization\tbme_submission_release_v1"
+echo Pushing to: git@github.com:espetinhosofiate367-cyber/Intraoperative-pulmonary-nodule-localization-and-feature-inversion-system.git
+git push -u origin main
+pause
